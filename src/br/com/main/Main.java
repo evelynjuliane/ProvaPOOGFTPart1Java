11e6eafc41578ddbed639af2f5895edfdc6b596a
Evelyn Juliane <evelynbr@live.com>
2020-01-15 13:37:27 -0300

    Clear

--- src/br/com/main/Main.java
+++ src/br/com/main/Main.java
@@ -11,43 +11,43 @@ public class Main {
 	public static void main(String[] args) {
 		
 		
-		ArrayList<Pessoa> list = new ArrayList<>();
-		
 		Pessoa p1 = new Pessoa("João", 15);
 		Pessoa p2 = new Pessoa("Leandro", 21);
 		Pessoa p3 = new Pessoa("Paulo", 17);
-		Pessoa p4 = new Pessoa("Jéssica", 18);
+		Pessoa p4 = new Pessoa("Jessica", 18);
+		
+		ArrayList<Pessoa> list = new ArrayList<>();
 		
 		list.add(p1);
 		list.add(p2);
 		list.add(p3);
 		list.add(p4);
 		
+		int aux = 0;
+		String saux = " ";
 		
 		for (Pessoa index : list) {
-			int aux = 0;
 			if(index.getIdade() > aux ) {
-				aux = index.getIdade();		
-			}
-			System.out.println(aux);
-		}
-		for (Pessoa index : list) {
-			System.out.println(index);
-		}
-		for (Pessoa index : list) {
-			if(index.getIdade() < 18 ) {
-				list.remove(index);
+				aux = index.getIdade();
+				saux = index.getNome();
 			}
 		}
-		for (Pessoa index : list) {
-			System.out.println(index);
+		System.out.println( saux + " " + aux);
+		
+		System.out.println(list.size());
+		
+		for (int i = 0; i < list.size(); i ++) {
+			if(list.get(i).getIdade() < 18) {
+				list.remove(i);
+			}
+		}
+		
+		System.out.println(list.size());
 
-		}
 		for (Pessoa index : list) {
 			if(index.getNome() == "Jessica") {
 				System.out.println(index.getIdade());
 			}
-			System.out.println("\nJessica não está na lista!\n");
 		}
 		
 	}
